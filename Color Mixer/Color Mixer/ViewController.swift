32125c53c7ab7ea3f2a1cc2a3eb09537d2db7ba8
Luis Felipe Salazar <sv.luisfelipe@gmail.com>
2015-03-21 15:37:44 -0600

    Proyecto
    Descr

---
+++ Color Mixer/Color Mixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Mixer
+//
+//  Created by Luis Felipe Salazar on 3/21/15.
+//  Copyright (c) 2015 Luis Felipe Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
